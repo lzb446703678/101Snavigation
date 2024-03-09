e1f24b15b0c0ede9f8c11740ec77c3c756fffe37
lzb446703678 <446703678@qq.com>
2024-03-09 13:41:10 +0800

    1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
       <span v-if="icp" class="icp" @click="jumpTo('https://beian.miit.gov.cn')">
         {{ icp }}
       </span>
-      <span class="about" @click="aboutSiteModal = true">关于</span>
+      <span class="about" @click="jumpTo('https://101jc.com')">返回主页</span>
     </div>
     <!-- 关于 -->
     <n-modal
